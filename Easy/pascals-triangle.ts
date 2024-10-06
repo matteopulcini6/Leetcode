75ef9dd6d3bfdef5e3f5e50ad540ddb0dfd91eab
Matteo Pulcini <Matteo.Pulcini@ibm.com>
2024-10-06 18:30:14 -0400

    Optimized Pascal's Triangle memory by avoiding redundant checks/assignments

--- Easy/pascals-triangle.ts
+++ Easy/pascals-triangle.ts
@@ -1,26 +1,22 @@
 function generate(numRows: number): number[][] {
   let result: number[][] = [];
-  let base_1 = [1];
-  let base_2 = [1,1];
 
   if(numRows === 0) return [];
-  else if(numRows === 1){return [base_1];}
+  else if(numRows === 1){return [[1]];}
   
-  result.push(base_1, base_2);
+  result.push([1], [1,1]);
   
   if(numRows === 2){ return result;}
 
-  let last_array = base_2;
   for(let i = 2; i < numRows; i++){
       let currentArray:number[] = [1];
+      let last_array = result[i - 1];
 
       for(let j = 0; j < i - 1; j++){
           currentArray.push(last_array[j] + last_array[j+1])
       }
       currentArray.push(1);
       result.push(currentArray);
-
-      last_array = currentArray;
   }
 
   return result;
