d0e2bbc3164087d6202d8100e1783acb35268686
Matteo Pulcini <Matteo.Pulcini@ibm.com>
2024-10-06 14:35:13 -0400

    Removed TreeNode class

--- Easy/same_tree.ts
+++ Easy/same_tree.ts
@@ -1,15 +1,3 @@
-// class TreeNode {
-//     val: number
-//     left: TreeNode | null
-//     right: TreeNode | null
-//     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-//         this.val = (val===undefined ? 0 : val)
-//         this.left = (left===undefined ? null : left)
-//         this.right = (right===undefined ? null : right)
-//     }
-// }
-
-
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
   if(p === null && q === null) return true;
   else if(p === null && q !== null) return false;
